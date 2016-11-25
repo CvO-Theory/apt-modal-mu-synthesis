3a86677c18b0efdb7e9b906bddbf5a2200d5f001
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2016-11-25 11:13:58 +0100

    Implement toString() for FixedPoint
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- src/uniol/synthesis/adt/mu_calculus/FixedPoint.java
+++ src/uniol/synthesis/adt/mu_calculus/FixedPoint.java
@@ -1,6 +1,17 @@
 package uniol.synthesis.adt.mu_calculus;
 
 public enum FixedPoint {
-	LEAST,
-	GREATEST;
+	LEAST("mu"),
+	GREATEST("nu");
+
+	private final String toStringValue;
+
+	FixedPoint(String toStringValue) {
+		this.toStringValue = toStringValue;
+	}
+
+	@Override
+	public String toString() {
+		return toStringValue;
+	}
 }
