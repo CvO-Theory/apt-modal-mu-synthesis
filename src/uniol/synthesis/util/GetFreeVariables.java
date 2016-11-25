a3ae2b47d610b872f302effec53b0d4e845e7f19
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2016-11-25 16:08:32 +0100

    Add code for finding the free variables in a formula
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/uniol/synthesis/util/GetFreeVariables.java
@@ -0,0 +1,50 @@
+package uniol.synthesis.util;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.bag.HashBag;
+
+import uniol.synthesis.adt.mu_calculus.Formula;
+import uniol.synthesis.adt.mu_calculus.ConstantFormula;
+import uniol.synthesis.adt.mu_calculus.ConjunctionFormula;
+import uniol.synthesis.adt.mu_calculus.DisjunctionFormula;
+import uniol.synthesis.adt.mu_calculus.NegationFormula;
+import uniol.synthesis.adt.mu_calculus.VariableFormula;
+import uniol.synthesis.adt.mu_calculus.ModalityFormula;
+import uniol.synthesis.adt.mu_calculus.FixedPointFormula;
+
+public class GetFreeVariables extends RecursiveFormulaWalker {
+	final private Set<VariableFormula> freeVariables = new HashSet<>();
+	final private Bag<VariableFormula> currentlyBoundVariables = new HashBag<>();
+
+	public GetFreeVariables(Formula formula) {
+		super(formula);
+	}
+
+	public Set<VariableFormula> getFreeVariables() {
+		return Collections.unmodifiableSet(freeVariables);
+	}
+
+	protected void visit(NonRecursive engine, VariableFormula formula) {
+		if (!currentlyBoundVariables.contains(formula))
+			freeVariables.add(formula);
+	}
+
+	protected void enter(NonRecursive engine, FixedPointFormula formula) {
+		currentlyBoundVariables.add(formula.getVariable());
+	}
+
+	protected void exit(NonRecursive engine, FixedPointFormula formula) {
+		boolean changed = currentlyBoundVariables.remove(formula.getVariable(), 1);
+		assert changed;
+	}
+
+	static public Set<VariableFormula> getFreeVariables(Formula formula) {
+		GetFreeVariables gfv = new GetFreeVariables(formula);
+		new NonRecursive().run(gfv);
+		return gfv.getFreeVariables();
+	}
+}
---
+++ src/uniol/synthesis/util/RecursiveFormulaWalker.java
@@ -0,0 +1,142 @@
+package uniol.synthesis.util;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+import uniol.synthesis.adt.mu_calculus.Formula;
+import uniol.synthesis.adt.mu_calculus.ConstantFormula;
+import uniol.synthesis.adt.mu_calculus.ConjunctionFormula;
+import uniol.synthesis.adt.mu_calculus.DisjunctionFormula;
+import uniol.synthesis.adt.mu_calculus.NegationFormula;
+import uniol.synthesis.adt.mu_calculus.VariableFormula;
+import uniol.synthesis.adt.mu_calculus.ModalityFormula;
+import uniol.synthesis.adt.mu_calculus.FixedPointFormula;
+
+public abstract class RecursiveFormulaWalker implements NonRecursive.Walker {
+	final protected Formula startFormula;
+
+	public RecursiveFormulaWalker(Formula formula) {
+		this.startFormula = formula;
+	}
+
+	@Override
+	final public void walk(NonRecursive engine) {
+		engine.enqueue(new RecursiveWalker(startFormula));
+	}
+
+	private class RecursiveWalker extends FormulaWalker {
+		private boolean enter = true;
+
+		public RecursiveWalker(Formula formula) {
+			super(formula);
+		}
+
+		@Override
+		public void walk(NonRecursive engine, ConstantFormula formula) {
+			RecursiveFormulaWalker.this.visit(engine, formula);
+		}
+
+		@Override
+		public void walk(NonRecursive engine, VariableFormula formula) {
+			RecursiveFormulaWalker.this.visit(engine, formula);
+		}
+
+		@Override
+		public void walk(NonRecursive engine, ConjunctionFormula formula) {
+			if (enter) {
+				enter = false;
+				RecursiveFormulaWalker.this.enter(engine, formula);
+				engine.enqueue(this);
+				engine.enqueue(new RecursiveWalker(formula.getRight()));
+				engine.enqueue(new RecursiveWalker(formula.getLeft()));
+			} else {
+				RecursiveFormulaWalker.this.exit(engine, formula);
+			}
+		}
+
+		@Override
+		public void walk(NonRecursive engine, DisjunctionFormula formula) {
+			if (enter) {
+				enter = false;
+				RecursiveFormulaWalker.this.enter(engine, formula);
+				engine.enqueue(this);
+				engine.enqueue(new RecursiveWalker(formula.getRight()));
+				engine.enqueue(new RecursiveWalker(formula.getLeft()));
+			} else {
+				RecursiveFormulaWalker.this.exit(engine, formula);
+			}
+		}
+
+		@Override
+		public void walk(NonRecursive engine, NegationFormula formula) {
+			if (enter) {
+				enter = false;
+				RecursiveFormulaWalker.this.enter(engine, formula);
+				engine.enqueue(this);
+				engine.enqueue(new RecursiveWalker(formula.getFormula()));
+			} else {
+				RecursiveFormulaWalker.this.exit(engine, formula);
+			}
+		}
+
+		@Override
+		public void walk(NonRecursive engine, ModalityFormula formula) {
+			if (enter) {
+				enter = false;
+				RecursiveFormulaWalker.this.enter(engine, formula);
+				engine.enqueue(this);
+				engine.enqueue(new RecursiveWalker(formula.getFormula()));
+			} else {
+				RecursiveFormulaWalker.this.exit(engine, formula);
+			}
+		}
+
+		@Override
+		public void walk(NonRecursive engine, FixedPointFormula formula) {
+			if (enter) {
+				enter = false;
+				RecursiveFormulaWalker.this.enter(engine, formula);
+				engine.enqueue(this);
+				engine.enqueue(new RecursiveWalker(formula.getFormula()));
+			} else {
+				RecursiveFormulaWalker.this.exit(engine, formula);
+			}
+		}
+	}
+
+	protected void visit(NonRecursive engine, ConstantFormula formula) {
+	}
+
+	protected void visit(NonRecursive engine, VariableFormula formula) {
+	}
+
+	protected void enter(NonRecursive engine, ConjunctionFormula formula) {
+	}
+
+	protected void exit(NonRecursive engine, ConjunctionFormula formula) {
+	}
+
+	protected void enter(NonRecursive engine, DisjunctionFormula formula) {
+	}
+
+	protected void exit(NonRecursive engine, DisjunctionFormula formula) {
+	}
+
+	protected void enter(NonRecursive engine, NegationFormula formula) {
+	}
+
+	protected void exit(NonRecursive engine, NegationFormula formula) {
+	}
+
+	protected void enter(NonRecursive engine, ModalityFormula formula) {
+	}
+
+	protected void exit(NonRecursive engine, ModalityFormula formula) {
+	}
+
+	protected void enter(NonRecursive engine, FixedPointFormula formula) {
+	}
+
+	protected void exit(NonRecursive engine, FixedPointFormula formula) {
+	}
+}
