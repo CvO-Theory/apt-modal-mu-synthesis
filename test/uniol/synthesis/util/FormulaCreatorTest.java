09644b5b9f97a859b5eb0533fdb4851e1c772016
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-04-24 13:15:40 +0200

    Add a unit testing infrastructure
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ test/uniol/synthesis/util/FormulaCreatorTest.java
@@ -0,0 +1,12 @@
+package uniol.synthesis.util;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class FormulaCreatorTest {
+	@Test
+	public void testFoo() {
+		assertThat(true, is(true));
+	}
+}
