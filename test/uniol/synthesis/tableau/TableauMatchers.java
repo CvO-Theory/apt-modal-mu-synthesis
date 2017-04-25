880d9e4fcd4c1cbb59938464f149eb1b7d3591e2
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-04-25 18:41:58 +0200

    Add some matchers
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- test/uniol/synthesis/tableau/TableauMatchers.java
+++ test/uniol/synthesis/tableau/TableauMatchers.java
@@ -1,7 +1,14 @@
 package uniol.synthesis.tableau;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
+import org.hamcrest.Description;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 import static org.hamcrest.Matchers.*;
 
 import uniol.synthesis.adt.mu_calculus.Formula;
@@ -31,6 +38,53 @@ public class TableauMatchers {
 			@Override
 			protected Formula featureValueOf(TableauNode node) {
 				return node.getFormula();
+			}
+		};
+	}
+
+	static public Matcher<TableauNode> hasFormula(Formula formula) {
+		return hasFormula(equalTo(formula));
+	}
+
+	static public Matcher<Tableau> hasLeaves(final Matcher<Set<TableauNode>> nodesMatcher) {
+		return new FeatureMatcher<Tableau, Set<TableauNode>>(nodesMatcher, "getLeaves", "getLeaves") {
+			@Override
+			protected Set<TableauNode> featureValueOf(Tableau node) {
+				return node.getLeaves();
+			}
+		};
+	}
+
+	static public Matcher<Set<Tableau>> hasNSuccessfulTableaus(final int n) {
+		return new TypeSafeDiagnosingMatcher<Set<Tableau>>() {
+			@Override
+			public void describeTo(Description description) {
+				description.appendValue(n).appendText(" successful tableaus");
+			}
+
+			@Override
+			protected boolean matchesSafely(Set<Tableau> tableaus, Description mismatch) {
+				boolean result = true;
+
+				if (tableaus.size() != n) {
+					mismatch.appendText(" was ").appendValue(tableaus.size()).appendText(" tableaus");
+					result = false;
+				}
+
+				Set<Tableau> error = new HashSet<>();
+				for (Tableau tableau : tableaus) {
+					for (TableauNode leave : tableau.getLeaves())
+						if (!leave.isSuccessful()) {
+							error.add(tableau);
+							break;
+						}
+				}
+				if (!error.isEmpty()) {
+					mismatch.appendText(" not successful: ").appendValue(error);
+					result = false;
+				}
+
+				return result;
 			}
 		};
 	}
