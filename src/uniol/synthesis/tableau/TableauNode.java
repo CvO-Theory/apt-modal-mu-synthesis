45e72daf08b2281bcdf9444c55198e491bad5acb
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-04-25 15:46:49 +0200

    Some more work on the new TableauNode
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- src/uniol/synthesis/tableau/TableauNode.java
+++ src/uniol/synthesis/tableau/TableauNode.java
@@ -2,7 +2,10 @@ package uniol.synthesis.tableau;
 
 import uniol.apt.adt.ts.State;
 
+import uniol.synthesis.adt.mu_calculus.ConstantFormula;
 import uniol.synthesis.adt.mu_calculus.Formula;
+import uniol.synthesis.adt.mu_calculus.Modality;
+import uniol.synthesis.adt.mu_calculus.ModalityFormula;
 
 public class TableauNode {
 	private State state;
@@ -13,11 +16,41 @@ public class TableauNode {
 		this.formula = formula;
 	}
 
+	/// Return true if this node as a leave corresponds to a successful tableau
+	public boolean isSuccessful() {
+		if (formula instanceof ModalityFormula) {
+			ModalityFormula modf = (ModalityFormula) formula;
+			return modf.getModality().equals(Modality.UNIVERSAL) &&
+				state.getPostsetEdgesByLabel(modf.getEvent().getLabel()).isEmpty();
+		}
+		if (formula instanceof ConstantFormula) {
+			return ((ConstantFormula) formula).getValue();
+		}
+		return false;
+	}
+
 	public State getState() {
 		return state;
 	}
 
 	public Formula getFormula() {
 		return formula;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 0;
+		result = result * 37 + state.hashCode();
+		result = result * 37 + formula.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof TableauNode))
+			return false;
+		TableauNode other = (TableauNode) o;
+		return this.state.equals(other.state) &&
+			this.formula.equals(other.formula);
 	}
 }
--- test/uniol/synthesis/tableau/TableauNodeTest.java
+++ test/uniol/synthesis/tableau/TableauNodeTest.java
@@ -3,9 +3,15 @@ package uniol.synthesis.tableau;
 import uniol.apt.adt.ts.State;
 import uniol.apt.adt.ts.TransitionSystem;
 
+import uniol.synthesis.adt.mu_calculus.Event;
+import uniol.synthesis.adt.mu_calculus.FixedPoint;
 import uniol.synthesis.adt.mu_calculus.Formula;
 import uniol.synthesis.adt.mu_calculus.FormulaCreator;
+import uniol.synthesis.adt.mu_calculus.Modality;
+import uniol.synthesis.adt.mu_calculus.VariableFormula;
 
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -21,6 +27,15 @@ public class TableauNodeTest {
 		return ts.getNode("s0");
 	}
 
+	private Matcher<TableauNode> isSuccessful(final boolean expected) {
+		return new FeatureMatcher<TableauNode, Boolean>(equalTo(expected), "isSuccessful", "isSuccessful") {
+			@Override
+			protected Boolean featureValueOf(TableauNode node) {
+				return node.isSuccessful();
+			}
+		};
+	}
+
 	@Test
 	public void testConstructor() {
 		FormulaCreator creator = new FormulaCreator();
@@ -30,5 +45,42 @@ public class TableauNodeTest {
 		TableauNode node = new TableauNode(state, formula);
 		assertThat(node.getState(), sameInstance(state));
 		assertThat(node.getFormula(), sameInstance(formula));
+	}
+
+	@Test
+	public void testEquals() {
+		FormulaCreator creator = new FormulaCreator();
+		State state = getABCState();
+
+		TableauNode node1 = new TableauNode(state, creator.constant(true));
+		assertThat(node1, not(equalTo((Object) "foo")));
+		assertThat(new TableauNode(state, creator.constant(true)), equalTo(node1));
+		assertThat(new TableauNode(state, creator.constant(true)).hashCode(), equalTo(node1.hashCode()));
+
+		assertThat(new TableauNode(state, creator.constant(false)), not(equalTo(node1)));
+
+		state = getABCState();
+		assertThat(new TableauNode(state, creator.constant(true)), not(equalTo(node1)));
+	}
+
+	@Test
+	public void testSuccessful() {
+		FormulaCreator creator = new FormulaCreator();
+		State state = getABCState();
+		VariableFormula someFormula = creator.variable("X");
+
+		assertThat(new TableauNode(state, creator.constant(false)), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.constant(true)), isSuccessful(true));
+		assertThat(new TableauNode(state, creator.negate(someFormula)), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.conjunction(someFormula, someFormula)), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.disjunction(someFormula, someFormula)), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.variable("X")), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.fixedPoint(FixedPoint.LEAST, someFormula, someFormula)), isSuccessful(false));
+
+		assertThat(new TableauNode(state, creator.modality(Modality.EXISTENTIAL, new Event("z"), someFormula)), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.modality(Modality.UNIVERSAL,   new Event("z"), someFormula)), isSuccessful(true));
+
+		assertThat(new TableauNode(state, creator.modality(Modality.EXISTENTIAL, new Event("a"), someFormula)), isSuccessful(false));
+		assertThat(new TableauNode(state, creator.modality(Modality.UNIVERSAL,   new Event("a"), someFormula)), isSuccessful(false));
 	}
 }
