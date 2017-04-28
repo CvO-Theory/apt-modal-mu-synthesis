39f1e387310206ad4099982f625e940da8036001
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-04-28 16:44:32 +0200

    Add LICENSE, licenese headers and vim modelines
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- test/uniol/synthesis/adt/mu_calculus/FormulaFactoryTest.java
+++ test/uniol/synthesis/adt/mu_calculus/FormulaFactoryTest.java
@@ -1,3 +1,22 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2017  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package uniol.synthesis.adt.mu_calculus;
 
 import java.util.ArrayList;
@@ -196,3 +215,5 @@ public class FormulaFactoryTest {
 		assertThat(VariableFormula.variable(creator, a2), sameInstance(expected));
 	}
 }
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
