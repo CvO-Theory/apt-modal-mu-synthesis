a091afc240c52ead8d2f0077495b02c60f6c72a8
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-07-06 15:01:18 +0200

    Add some code to compute the alphabet of a formula
    The alphabet contains all events that appear inside of modalities inside
    of the formula.
    
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/uniol/synthesis/util/AlphabetFinder.java
@@ -0,0 +1,112 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2017  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.synthesis.util;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import uniol.synthesis.adt.mu_calculus.ConjunctionFormula;
+import uniol.synthesis.adt.mu_calculus.ConstantFormula;
+import uniol.synthesis.adt.mu_calculus.DisjunctionFormula;
+import uniol.synthesis.adt.mu_calculus.FixedPointFormula;
+import uniol.synthesis.adt.mu_calculus.Formula;
+import uniol.synthesis.adt.mu_calculus.ModalityFormula;
+import uniol.synthesis.adt.mu_calculus.NegationFormula;
+import uniol.synthesis.adt.mu_calculus.VariableFormula;
+
+public class AlphabetFinder extends FormulaTransformer<Set<String>> {
+	@Override
+	protected void enqueueWalker(NonRecursive engine, Formula formula) {
+		engine.enqueue(new Worker(formula));
+	}
+
+	private class Worker extends FormulaTransformer<Set<String>>.FillCache {
+		private Worker(Formula formula) {
+			super(formula);
+		}
+
+		private Set<String> union(Set<String> set1, Set<String> set2) {
+			Set<String> result = new HashSet<>();
+			result.addAll(set1);
+			result.addAll(set2);
+			return result;
+		}
+
+		@Override
+		public void walk(NonRecursive engine, ConstantFormula formula) {
+			setCache(formula, Collections.<String>emptySet());
+		}
+
+		@Override
+		public void walk(NonRecursive engine, VariableFormula formula) {
+			setCache(formula, Collections.<String>emptySet());
+		}
+
+		@Override
+		public Set<String> conjunction(ConjunctionFormula formula, Set<String> transformedLeft,
+				Set<String> transformedRight) {
+			return union(transformedLeft, transformedRight);
+		}
+
+		@Override
+		public Set<String> disjunction(DisjunctionFormula formula, Set<String> transformedLeft,
+				Set<String> transformedRight) {
+			return union(transformedLeft, transformedRight);
+		}
+
+		@Override
+		public Set<String> negate(NegationFormula formula, Set<String> transformedChild) {
+			return transformedChild;
+		}
+
+		@Override
+		public Set<String> modality(ModalityFormula formula, Set<String> transformedChild) {
+			if (transformedChild.contains(formula.getEvent()))
+				return transformedChild;
+			Set<String> result = new HashSet<>(transformedChild);
+			result.add(formula.getEvent());
+			return result;
+		}
+
+		@Override
+		public Set<String> fixedPoint(FixedPointFormula formula, Set<String> transformedChild) {
+			return transformedChild;
+		}
+	}
+
+	@Override
+	public Set<String> transform(NonRecursive engine, Formula formula) {
+		Set<String> result = super.transform(engine, formula);
+		if (result != null)
+			result = Collections.unmodifiableSet(result);
+		return result;
+	}
+
+	static public Set<String> getAlphabet(Formula formula) {
+		NonRecursive engine = new NonRecursive();
+		AlphabetFinder worker = new AlphabetFinder();
+		worker.transform(engine, formula);
+		engine.run();
+		return worker.transform(engine, formula);
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
---
+++ test/uniol/synthesis/util/AlphabetFinderTest.java
@@ -0,0 +1,93 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2017  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.synthesis.util;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import uniol.synthesis.adt.mu_calculus.FixedPoint;
+import uniol.synthesis.adt.mu_calculus.Formula;
+import uniol.synthesis.adt.mu_calculus.FormulaCreator;
+import uniol.synthesis.adt.mu_calculus.Modality;
+import uniol.synthesis.adt.mu_calculus.VariableFormula;
+
+public class AlphabetFinderTest {
+	static private FormulaCreator creator = new FormulaCreator();
+
+	@Test
+	public void testConstantFormula() {
+		Formula formula = creator.constant(true);
+		assertThat(AlphabetFinder.getAlphabet(formula), empty());
+	}
+
+	@Test
+	public void testVariableFormula() {
+		Formula formula = creator.variable("foo");
+		assertThat(AlphabetFinder.getAlphabet(formula), empty());
+	}
+
+	@Test
+	public void testFixedPointFormula() {
+		Formula True = creator.constant(true);
+		Formula formula = creator.fixedPoint(FixedPoint.LEAST, creator.variable("foo"), True);
+		assertThat(AlphabetFinder.getAlphabet(formula), empty());
+	}
+
+	@Test
+	public void testFixedPointFormula2() {
+		VariableFormula var = creator.variable("foo");
+		Formula formula = creator.fixedPoint(FixedPoint.LEAST, var, var);
+		assertThat(AlphabetFinder.getAlphabet(formula), empty());
+	}
+
+	@Test
+	public void testModality1() {
+		Formula formula = creator.modality(Modality.EXISTENTIAL, "a", creator.constant(true));
+		assertThat(AlphabetFinder.getAlphabet(formula), contains("a"));
+	}
+
+	@Test
+	public void testModality2() {
+		Formula formula = creator.negate(creator.modality(Modality.EXISTENTIAL, "a", creator.constant(true)));
+		formula = creator.conjunction(formula, creator.modality(Modality.UNIVERSAL, "b", creator.variable("X")));
+		assertThat(AlphabetFinder.getAlphabet(formula), containsInAnyOrder("a", "b"));
+	}
+
+	@Test
+	public void testModality3() {
+		Formula formula = creator.modality(Modality.EXISTENTIAL, "a",
+				creator.disjunction(
+					creator.modality(Modality.UNIVERSAL, "b", creator.constant(false)),
+					creator.modality(Modality.EXISTENTIAL, "a", creator.constant(true))));
+		assertThat(AlphabetFinder.getAlphabet(formula), containsInAnyOrder("a", "b"));
+	}
+
+	@Test
+	public void testCache() {
+		Formula formula = creator.modality(Modality.EXISTENTIAL, "a", creator.constant(true));
+		for (int i = 0; i < 1000; i++)
+			formula = creator.conjunction(formula, formula);
+		// There are now 2^1000 occurrences of the modality in the formula
+		assertThat(AlphabetFinder.getAlphabet(formula), contains("a"));
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
