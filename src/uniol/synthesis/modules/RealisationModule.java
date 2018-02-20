7bf68c48fb37790324367a2f3643dbf2eb69ab33
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2018-02-20 14:15:28 +0100

    Do some hacks to make RealisationModule write directly to stdout
    This allows to see results as soon as they are produced.
    
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- src/uniol/synthesis/modules/RealisationModule.java
+++ src/uniol/synthesis/modules/RealisationModule.java
@@ -20,15 +20,12 @@
 package uniol.synthesis.modules;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.Writer;
 
 import uniol.apt.adt.ts.State;
 import uniol.apt.adt.ts.TransitionSystem;
 import uniol.apt.analysis.synthesize.AbstractSynthesizeModule;
 import uniol.apt.analysis.synthesize.PNProperties;
-import uniol.apt.io.renderer.RenderException;
 import uniol.apt.io.renderer.impl.AptLTSRenderer;
 import uniol.apt.module.AbstractModule;
 import uniol.apt.module.AptModule;
@@ -39,11 +36,9 @@ import uniol.apt.module.ModuleInputSpec;
 import uniol.apt.module.ModuleOutput;
 import uniol.apt.module.ModuleOutputSpec;
 import uniol.apt.module.exception.ModuleException;
-import uniol.apt.util.Pair;
 import uniol.synthesis.adt.mu_calculus.Formula;
 import uniol.synthesis.expansion.RealiseFormula;
 import uniol.synthesis.tableau.Tableau;
-import uniol.synthesis.util.NonRecursive;
 
 @AptModule
 public class RealisationModule extends AbstractModule implements Module {
@@ -65,40 +60,53 @@ public class RealisationModule extends AbstractModule implements Module {
 
 	@Override
 	public void provide(ModuleOutputSpec outputSpec) {
-		outputSpec.addReturnValue("result", String.class,
+		outputSpec.addReturnValue("result", DelayedWork.class,
 				ModuleOutputSpec.PROPERTY_FILE, ModuleOutputSpec.PROPERTY_RAW);
 	}
 
 	@Override
 	public void run(ModuleInput input, ModuleOutput output) throws ModuleException {
 		String options = input.getParameter("options", String.class);
-		PNProperties properties = AbstractSynthesizeModule.Options.parseProperties(options).properties;
-		Formula formula = input.getParameter("formula", Formula.class);
+		final PNProperties properties = AbstractSynthesizeModule.Options.parseProperties(options).properties;
+		final Formula formula = input.getParameter("formula", Formula.class);
 
 		if (!properties.isKBounded())
 			System.err.println(
 					"Warning: Without requiring k-boundedness this algorithm might not terminate!");
 
-		final StringWriter resultString = new StringWriter();
-		final String[] separator = new String[] { "" };
-
-		new RealiseFormula(properties, new RealiseFormula.RealisationCallback() {
+		DelayedWork result = new DelayedWork() {
 			@Override
-			public void foundRealisation(TransitionSystem ts, Tableau<State> tableau) {
-				resultString.append(separator[0]);
+			public void generateOutput(final Writer writer) throws IOException, ModuleException {
 				try {
-					new AptLTSRenderer().render(ts, resultString);
-				} catch (RenderException e) {
-					throw new AssertionError("The AptLTSRenderer should not throw RenderException", e);
-				} catch (IOException e) {
-					throw new AssertionError("A StringWriter should not throw IOException", e);
+					new RealiseFormula(properties, new RealiseFormula.RealisationCallback() {
+						private boolean first = true;
+
+						@Override
+						public void foundRealisation(TransitionSystem ts, Tableau<State> tableau) {
+							try {
+								if (!first)
+									writer.write("\n\n===========\n\n");
+								first = false;
+
+								new AptLTSRenderer().render(ts, writer);
+								writer.flush();
+							} catch (IOException|ModuleException e) {
+								throw new RuntimeException(e);
+							}
+						}
+					}).realise(formula);
+				} catch (RuntimeException e) {
+					Throwable cause = e.getCause();
+					if (cause instanceof IOException)
+						throw (IOException) cause;
+					if (cause instanceof ModuleException)
+						throw (ModuleException) cause;
+					throw e;
 				}
-
-				separator[0] = "\n\n===========\n\n";
 			}
-		}).realise(formula);
+		};
 
-		output.setReturnValue("result", String.class, resultString.toString());
+		output.setReturnValue("result", DelayedWork.class, result);
 	}
 
 	@Override
